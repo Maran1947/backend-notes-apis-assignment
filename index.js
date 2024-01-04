6bd65a00164b09d5ff10f1b77dfc711d8e9dcb5e
Abhishek Maran <abhishekmaran1947@gmail.com>
2024-01-04 14:55:24 +0530

    Added Auth Apis

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config();
 require('./src/db/dbconn.js');
+
+const routes = require('./src/api/routes/routes.js');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());  
+app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send("Welcome to Notes APIs!");
 });
+
+app.use('/api',routes);
 
 app.get('*', (req,res) => {
     res.status(404).send({
