bb0392680d85c4fb7c5f18a5fd7bdcb722824c13
Abhishek Maran <abhishekmaran1947@gmail.com>
2024-01-04 17:59:59 +0530

    added rate limiting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit')
 require('dotenv').config();
 require('./src/db/dbconn.js');
 
@@ -9,17 +10,28 @@ const routes = require('./src/api/routes/routes.js');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.json());  
+const apiRequestLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+    handler: (req,res) => {
+        return res.status(429).json({
+            message: "Too many requests, please try again later."
+        });
+    }
+});
+
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(apiRequestLimiter);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("Welcome to Notes APIs!");
 });
 
-app.use('/api',routes);
+app.use('/api', routes);
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.status(404).send({
         message: "Not found",
         path: req.path
